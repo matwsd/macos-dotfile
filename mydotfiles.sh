acce0caafb0e72dc3280effe8bc38e93d7b0b2ca
mattia <mattiaborca@gmail.com>
2026-03-20 12:24:21 +0100

    Updated dotfiles:

---
+++ mydotfiles.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+DOT_DIR="$HOME/macos-dotfiles"
+CONF_DIR="$HOME/.config"
+
+FILES=(
+	"$HOME/.bashrc"
+	"$HOME/.bash_profile"
+)
+
+DIR_CONF=(
+	"$CONF_DIR"
+	"$HOME/bin"
+)
+echo "Updating dotfiles.."
+
+for file in "${FILES[@]}"; do
+	if [ -f "$file" ]; then
+		cp -R "$file" "$DOT_DIR"
+		echo "Updated: $file"
+	else
+		echo "Warning: $file not found"
+	fi
+done
+
+for dir in "${DIR_CONF[@]}"; do
+	if [ -d "$dir" ]; then
+		if [ -z "$(ls -A "$dir")"]; then
+			echo "Skip: $dir is empty"
+			continue
+		fi
+		cp -R "$dir" "$DOT_DIR"
+		echo "Updated:" "$(tree $dir)"
+	else
+		echo "Warning: no file found in $dir"
+	fi
+done
+
+
+if [[ -n $(git status -s) ]]; then
+	echo "Changes detected. Push to Git"
+
+	git add .
+	git commit -m "Updated dotfiles: $(date + '%Y-%m-%d %H:%M:%S')"
+	git push origin main
+
+	echo "Pushed!"
+else
+	echo "Nothing changed."
+fi
+
